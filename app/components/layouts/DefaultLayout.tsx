8215496a22833904a6c7daf8e6ea5b766c323ac1
ppm-tjohann <tjohann@pp-m.net>
2022-07-10 10:10:16 +0200

    project init

---
+++ app/components/layouts/DefaultLayout.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DefaultLayout: React.FC = () => {
+  return (
+      <h1>DefaultLayout</h1>
+  )
+}
+export default DefaultLayout
---
+++ app/pages/about.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from 'next'
+import DefaultLayout from '../components/layouts/DefaultLayout'
+
+const about: NextPage = () => {
+  return (
+      <>
+        <DefaultLayout/>
+        <h2>Hallo Welt</h2>
+      </>
+  )
+}
+
+export default about
